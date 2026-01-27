dd454c182cb4e1363c7d23a584bcd83018768e78
Doru Blânzeanu <dblnz@pm.me>
2026-01-27 21:11:13 +0200

    Re-export hyperlight-host::Result for ease of use downstream
    Signed-off-by: Doru Blânzeanu <dblnz@pm.me>

--- src/hyperlight-python/src/lib.rs
+++ src/hyperlight-python/src/lib.rs
@@ -1,5 +1,10 @@
 use hyperlight_host::func::HostFunction;
 
+/// Module for sandboxed Python execution.
 pub mod sandbox;
 
+/// Type alias for a host function that prints a string from the sandboxed environment.
 pub type HostPrintFn = HostFunction<i32, (String,)>;
+
+/// Re-exporting Result type from hyperlight_host for downstream usage.
+pub use hyperlight_host::Result;
